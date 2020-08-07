50bf2e651c89d09a1b21a49c4da5e4fb14c88e06
Michael Keaveny <michaelkeaveny0@gmail.com>
2020-08-07 19:16:12 +0100

    Added roles and seeded database

---
+++ CustomUserManagement/Data/ContextSeed.cs
@@ -0,0 +1,20 @@
+﻿using CustomUserManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomUserManagement.Data
+{
+    public class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Basic.ToString()));
+        }
+    }
+}
--- CustomUserManagement/Program.cs
+++ CustomUserManagement/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CustomUserManagement.Data;
+using CustomUserManagement.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,11 +15,29 @@ namespace CustomUserManagement
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await ContextSeed.SeedRolesAsync(userManager, roleManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+            host.Run();
         }
-
+            
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
